26d8b82fdc119fee98259f816baa0fafff5e6ef9
rabea <julianqrooz@gmail.com>
2026-07-14 21:44:00 +0300

    cancel verify email and otp

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Layout from "./components/Layout";
-import VerifyEmail from "./pages/VerifyEmail";
+// import VerifyEmail from "./pages/VerifyEmail";
 import Profile from "./pages/Profile";
-import ForgotPassword from "./pages/ForgotPassword";
-import VerifyOTP from "./pages/VerifyOTP";
+// import ForgotPassword from "./pages/ForgotPassword";
+// import VerifyOTP from "./pages/VerifyOTP";
 import ResetPassword from "./pages/ResetPassword";
 import AdminUsers from "./pages/admin/AdminUsers";
 import AdminRoute from "@/components/AdminRoute";
@@ -58,6 +58,7 @@ function App() {
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/orders" element={<MyOrders />} />
             <Route path="/orders/:id" element={<OrderDetails />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
           </Route>
           {/* admain */}
           <Route element={<AdminRoute />}>
@@ -74,10 +75,10 @@ function App() {
 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/verify/:token" element={<VerifyEmail />} />
+        {/* <Route path="/verify/:token" element={<VerifyEmail />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/verify-otp" element={<VerifyOTP />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/verify-otp" element={<VerifyOTP />} /> */}
+
         {/* 👈 المسار الجديد */}
       </Routes>
     </>
